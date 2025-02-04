2a0702ffe3d6443a3460c3b7ba8290443585299b
Martin Köhler <info@martinjankoehler.com>
2025-02-04 19:21:10 +0100

    Implement overlap cap using PolygonNeighborhoodVisitor

--- klayout_pex/rcx25/extractor.py
+++ klayout_pex/rcx25/extractor.py
@@ -44,7 +44,8 @@ from ..tech_info import TechInfo
 from .extraction_results import *
 from .extraction_reporter import ExtractionReporter
 from .geometry_restorer import GeometryRestorer
-from .types import EdgeInterval, EdgeNeighborhood, EdgeNeighborhoodChild, EdgeNeighborhoodChildKind
+from .types import EdgeInterval, EdgeNeighborhood, EdgeNeighborhoodChild, EdgeNeighborhoodChildKind, \
+                   PolygonNeighborhood
 import klayout_pex_protobuf.process_stack_pb2 as process_stack_pb2
 
 
@@ -131,6 +132,52 @@ class RCExtractor:
 
         # ------------------------------------------------------------------------
 
+        def emit_overlap(bot_layer_name: LayerName,
+                         top_layer_name: LayerName,
+                         polygon_bot: kdb.PolygonWithProperties,
+                         polygon_top: kdb.PolygonWithProperties,
+                         shielded_region: kdb.Region):
+            top_overlap_specs = self.tech_info.overlap_cap_by_layer_names.get(top_layer_name, None)
+            if not top_overlap_specs:
+                warning(f"No overlap cap specified for layer top={top_layer_name}")
+                return
+            overlap_cap_spec = top_overlap_specs.get(bot_layer_name, None)
+            if not overlap_cap_spec:
+                warning(f"No overlap cap specified for layer bottom={bot_layer_name}")
+                return
+
+            net_bot = polygon_bot.property('net')
+            net_top = polygon_top.property('net')
+
+            top_region = kdb.Region(polygon_top)
+            bottom_region = kdb.Region(polygon_bot)
+
+            overlap_area = top_region.__and__(bottom_region) - shielded_region
+
+            overlap_area_um2 = overlap_area.area() * dbu ** 2
+            cap_femto = overlap_area_um2 * overlap_cap_spec.capacitance / 1000.0
+            info(f"(Overlap): {top_layer_name}({net_top})-{bot_layer_name}({net_bot}): "
+                 f"cap: {round(cap_femto, 2)} fF, "
+                 f"area: {overlap_area_um2} µm^2")
+
+            if cap_femto > 0.0:
+                ovk = OverlapKey(layer_top=top_layer_name,
+                                 net_top=net_top,
+                                 layer_bot=bot_layer_name,
+                                 net_bot=net_bot)
+                cap = OverlapCap(key=ovk,
+                                 cap_value=cap_femto,
+                                 shielded_area=0.0,  # TODO shielded_area_um2,
+                                 unshielded_area=0.0,  # TODO unshielded_area_um2,
+                                 tech_spec=overlap_cap_spec)
+
+                results.add_overlap_cap(cap)
+
+                report.output_overlap(overlap_cap=cap,
+                                      bottom_polygon=polygon_bot,
+                                      top_polygon=polygon_top,
+                                      overlap_area=overlap_area)
+
         def emit_sidewall(layer_name: LayerName,
                           edge: kdb.EdgeWithProperties,
                           edge_interval: EdgeInterval,
@@ -192,6 +239,34 @@ class RCExtractor:
 
         # ------------------------------------------------------------------------
 
+        class PEXPolygonNeighborhoodVisitor(kdb.PolygonNeighborhoodVisitor):
+            def __init__(self,
+                         layer_names: List[LayerName],
+                         inside_layer_index: int):
+                super().__init__()
+                self.layer_names = layer_names
+                self.inside_layer_index = inside_layer_index
+
+            def neighbors(self,
+                          layout: kdb.Layout,
+                          cell: kdb.Cell,
+                          polygon: kdb.PolygonWithProperties,
+                          neighborhood: PolygonNeighborhood):
+                # We just look "upwards", as we don't want to count areas twice
+                for other_layer_index in range(self.inside_layer_index + 1, len(self.layer_names)):
+                    neighbor_polygons = neighborhood.get(other_layer_index, None)
+                    if neighbor_polygons is None:
+                        continue
+
+                    for neighbor_polygon in neighbor_polygons:
+                        # TODO: bot_region is loop invariant, cache
+                        emit_overlap(bot_layer_name=self.layer_names[self.inside_layer_index],
+                                     top_layer_name=self.layer_names[other_layer_index],
+                                     polygon_bot=polygon,
+                                     polygon_top=neighbor_polygon)
+
+        # ------------------------------------------------------------------------
+
         class PEXEdgeNeighborhoodVisitor(kdb.EdgeNeighborhoodVisitor):
             def __init__(self,
                          children_description: List[EdgeNeighborhoodChild]):
@@ -249,35 +324,63 @@ class RCExtractor:
                                                 polygon=polygon,
                                                 geometry_restorer=geometry_restorer)
 
+        layer_names = list(layer_regions_by_name.keys())
+        all_layer_regions = layer_regions_by_name.values()
 
-        for layer_name, layer_region in layer_regions_by_name.items():
-            other_layer_names = [oln for oln in layer_regions_by_name.keys() if oln != layer_name]
+        for idx, (layer_name, layer_region) in enumerate(layer_regions_by_name.items()):
+            other_layer_names = [oln for oln in layer_names if oln != layer_name]
+            other_layer_regions = [
+                r for ln, r in layer_regions_by_name.items()
+                if ln != layer_name
+            ]
 
-            visitor = PEXEdgeNeighborhoodVisitor(
+            #
+            # OVERLAP
+            #
+
+            ovl_visitor = PEXPolygonNeighborhoodVisitor(
+                layer_names=layer_names,
+                inside_layer_index=idx
+            )
+
+            # See comment above: as we use the layers in the stack order,
+            # the input index is also the metal layer index
+            ovl_children = [kdb.CompoundRegionOperationNode.new_secondary(r) for r in all_layer_regions]
+
+            # We don't use a distance - hence only true overlaps will be considered
+            ovl_node = kdb.CompoundRegionOperationNode.new_polygon_neighborhood(ovl_children, ovl_visitor)
+
+            layer_region.complex_op(ovl_node)
+
+            #
+            # SIDEWALL / FRINGE
+            #
+
+            en_visitor = PEXEdgeNeighborhoodVisitor(
                 children_description=[EdgeNeighborhoodChild(EdgeNeighborhoodChildKind.SIDEWALL, layer_name)] +
                                      [EdgeNeighborhoodChild(EdgeNeighborhoodChildKind.OTHER_LAYER, name)
                                       for name in other_layer_names],
             )
 
-            children = [
+            en_children = [
                 # kdb.CompoundRegionOperationNode.new_primary(),
                 kdb.CompoundRegionOperationNode.new_foreign()
             ]
-            children += [kdb.CompoundRegionOperationNode.new_secondary(layer_regions_by_name[other_layer_name])
-                         for other_layer_name in other_layer_names]
+            en_children += [kdb.CompoundRegionOperationNode.new_secondary(other_layer_region)
+                            for other_layer_region in other_layer_regions]
 
             side_halo_um = self.tech_info.tech.process_parasitics.side_halo
             side_halo_dbu = int(side_halo_um / dbu) + 1  # add 1 nm to halo
 
-            node = kdb.CompoundRegionOperationNode.new_edge_neighborhood(
-                children=children,
-                visitor=visitor,
+            en_node = kdb.CompoundRegionOperationNode.new_edge_neighborhood(
+                children=en_children,
+                visitor=en_visitor,
                 bext=0, # bext
                 eext=0, # eext,
                 din=0, # din
                 dout=side_halo_dbu # dout
             )
 
-            layer_region.complex_op(node)
+            layer_region.complex_op(en_node)
 
         return results
