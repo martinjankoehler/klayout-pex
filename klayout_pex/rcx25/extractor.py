cf30b0575594e2bdf23dfacf0b2490590b988431
Martin Köhler <info@martinjankoehler.com>
2025-03-07 22:52:55 +0100

    WIP: resistance extraction

--- klayout_pex/rcx25/extractor.py
+++ klayout_pex/rcx25/extractor.py
@@ -25,10 +25,13 @@
 
 import klayout.db as kdb
 
+from .r.conductance import Conductance
 from ..klayout.lvsdb_extractor import KLayoutExtractionContext, GDSPair
 from ..log import (
     debug,
-    warning
+    warning,
+    info,
+    subproc
 )
 from ..tech_info import TechInfo
 from .extraction_results import *
@@ -157,8 +160,9 @@ class RCExtractor:
             sidewall_and_fringe_extractor.extract()
 
         # ------------------------------------------------------------------------
-        rex = ResistorExtraction(b=self.delaunay_b, amax=self.delaunay_amax)
         if self.pex_mode.need_resistance():
+            rex = ResistorExtraction(b=self.delaunay_b, amax=self.delaunay_amax)
+
             c: kdb.Circuit = netlist.top_circuit()
             debug(f"found {c.pin_count()}pins")
 
@@ -170,8 +174,34 @@ class RCExtractor:
                 pins = self.pex_context.pins_of_layer(gds_pair)
                 labels = self.pex_context.labels_of_layer(gds_pair)
 
+                layer_sheet_resistance = self.tech_info.layer_resistance_by_layer_name[layer_name]
+
+                # TODO: include vias into graph!!!
+
                 resistor_networks = rex.extract(polygons=region, pins=pins, labels=labels)
+
+                subproc(f"Layer {layer_name}   (R_coeff = {layer_sheet_resistance.resistance}):")
                 for rn in resistor_networks:
-                    print(rn.to_string(True))
+                    # print(rn.to_string(True))
+                    subproc("\tNodes:")
+                    for node_id in rn.node_to_s.keys():
+                        loc = rn.locations[node_id]
+                        node_name = rn.node_names[node_id]
+                        subproc(f"\t\tNode {node_name} at {loc} ({loc.x * dbu} µm, {loc.y * dbu} µm)")
+
+                    subproc("\tResistors:")
+                    visited_resistors: Set[Conductance] = set()
+                    for node_id, resistors in rn.node_to_s.items():
+                        node_name = rn.node_names[node_id]
+                        for conductance, other_node_id in resistors:
+                            if conductance in visited_resistors:
+                                continue # we don't want to add it twice, only once per direction!
+                            visited_resistors.add(conductance)
+
+                            other_node_name = rn.node_names[other_node_id]
+                            ohm = layer_sheet_resistance.resistance / 1000.0 / conductance.cond
+                            # TODO: layer_sheet_resistance.corner_adjustment_fraction not yet used !!!
+                            subproc(f"\t\t{node_name} ↔︎ {other_node_name}: {round(ohm, 3)} Ω    (internally: {conductance.cond})")
+
 
         return results
